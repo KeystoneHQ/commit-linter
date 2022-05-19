c8444514991d95b035c4a118733ab41bf4dd5826
soralit <soralitria@gmail.com>
2022-05-19 15:33:04 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget https://github.com/KeystoneHQ/commit-linter/blob/master/commit-msg | mv .git/hooks
+wget https://github.com/KeystoneHQ/commit-linter/blob/master/commit-msg-linter.js | mv .git/hooks
